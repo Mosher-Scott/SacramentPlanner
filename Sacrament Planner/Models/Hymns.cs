2df6538dc608641a06d8dffb1b86772053bcb674
Rachel Bichsel <rachbichsel@gmail.com>
2020-04-01 09:36:00 -0600

    undo testing

--- Sacrament Planner/Models/Hymns.cs
+++ Sacrament Planner/Models/Hymns.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,9 +11,6 @@ namespace Sacrament_Planner.Models
         public string HymnName { get; set; }
 
         public int PageNumber { get; set; }
-
-        [NotMapped]
-        public SelectList HymnList { get; set; }
 
         // A hymn can be in multiple sacrament meetings
         public ICollection<Meetings> Meeting { get; set; }
